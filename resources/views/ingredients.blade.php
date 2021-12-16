4514c5e8ebcfab83cdf983535ef69aaa8c8ebf34
Andretreccia <andreabtreccia@gmail.com>
2021-12-16 17:21:43 +0100

    create a second page create a navbar with links to pages links pages with function route()

--- resources/views/ingredients.blade.php
+++ resources/views/ingredients.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
     <title>Document</title>
 </head>
 
@@ -15,19 +14,27 @@
     <header>
         <nav class="navbar navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="{{ url ('home')}}">
+                <a class="navbar-brand" href="{{ route ('home')}}">
                     <img src="http://www.ruralhack.org/wp-content/uploads/2018/01/Pizza-PNG-Picture.png" alt=""
                         width="30" height="24" class="d-inline-block align-text-top">
                     Pizza
                 </a>
-                <a class="nav-link" href="{{ url ('ingredients')}}">Ingredients</a>
+                <a class="nav-link" href="{{ route ('ingredients')}}">Ingredients</a>
             </div>
         </nav>
     </header>
-    <main class="p-5">
-        <div class="container text-center text-light">
-            <h1>{{$title}}</h1>
-            <p>{{$text}}</p>
+
+    <main>
+        <div class="container text-light d-flex">
+            <div>
+                <h1>{{$title}}</h1>
+                <ul>
+                    @foreach ($ingredients as $ingredient)
+                    <li>{{$ingredient}}</li>
+                    @endforeach
+                </ul>
+            </div>
+            <img src="http://www.isimcalabria.it/wp-content/uploads/2019/03/MARGHERITA-1030x763.png" alt="">
         </div>
     </main>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $title = 'Tutorial Pizza';
+    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sit dolorum, minus saepe impedit libero consectetur iure ea adipisci incidunt iusto molestias quisquam voluptatum, repudiandae alias aliquam nostrum corporis animi?';
+    return view('home', compact('title', 'text'));
+})->name('home');
+
+Route::get('/ingredients', function () {
     $title = 'Ingredienti per la Pizza';
     $ingredients = [
-        'Farina', 'Acqua', 'Lievito', 'Sale', 'Pomodoro', 'Mozzarella', 'Mozzarella'
+        'Farina', 'Acqua', 'Lievito', 'Sale', 'Pomodoro', 'Mozzarella',
     ];
-    return view('home', compact('title', 'ingredients'));
-});
+    return view('ingredients', compact('title', 'ingredients'));
+})->name('ingredients');
